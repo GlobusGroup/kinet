518d403abb4f3d7d7b627ff682657f986fb562b2
Gab <gabriele.soldati@globusgroup.com>
2023-08-25 16:53:54 +0100

    Revert "Merge updates"
    This reverts commit 55edce71e6ab2c1102fe394509a583f25f4df0df.

--- src/WrappedValue.js
+++ src/WrappedValue.js
@@ -1,9 +1,11 @@
 import Dep from './Dep'
 
+// WrappedValue is a class that wraps a value and makes it reactive.
 class WrappedValue {
 	constructor(value, kinetInsance, path) {
-		this.dep = new Dep()
+		this.dep = new Dep(kinetInsance)
 		this.kinet = kinetInsance
+		this.path = path
 		if (typeof value === 'object' || Array.isArray(value)) {
 			this.value = this.kinet.makeReactive(value, path)
 			this.overrideSetters()
@@ -12,52 +14,13 @@ class WrappedValue {
 		}
 		Object.defineProperty(this, 'forceUpdate', {
 			value: function () {
-				if (typeof this.value == 'function') {
-					this.dep.notify(this.value(this.kinet.data))
-				} else {
-					this.dep.notify(this.value)
-				}
-			},
+				this.dep.notify(this.value)
+			}
 		})
-		if (typeof value == 'function') {
-			//look into the function and find all the paths that it uses
-			const used = this.findUsedPaths(value)
-			//for each path that it uses, subscribe to it and force an update
-			used.forEach((path) => {
-				const value = this.kinet.values[path]
-
-				if (value) {
-					value.dep.depend(() => {
-						this.forceUpdate()
-					})
-				}
-			})
-		}
 	}
 
-	findUsedPaths(fn) {
-		const functionString = fn.toString()
-
-		// Extract the argument name
-		const argRegex = /\(([^)]+)\)/
-		const argMatch = argRegex.exec(functionString)
-		if (!argMatch) {
-			return [] // No argument found
-		}
-		const argName = argMatch[1]
-
-		// Extract the properties accessed on the argument
-		const regex = new RegExp(`(?:${argName}\.)+([a-zA-Z_$][0-9a-zA-Z_$]*)`, 'g')
-		const properties = new Set()
-
-		let match
-		while ((match = regex.exec(functionString)) !== null) {
-			properties.add(match[1])
-		}
-
-		return Array.from(properties)
-	}
-
+	// The `overrideSetters` method sets up overrides for methods on arrays such as `push`, `pop`, `splice`, `shift`, and `unshift`.
+	// These overrides call the original method, then call `this.dep.notify` with `value` as the parameter.
 	overrideSetters() {
 		var value = this.value
 		var self = this
@@ -77,11 +40,8 @@ class WrappedValue {
 		}
 	}
 
+	// The `get` method simply returns `this.value`.
 	get() {
-		if (typeof this.value == 'function') {
-			return this.value(this.kinet.data)
-		}
-
 		return this.value
 	}
 
